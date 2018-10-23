46d3f7b70eec03ea2fa579fba904671887147b85
Marc Carbonell <karbu96@gmail.com>
2018-10-23 10:09:32 +0200

    ,.

--- Assets/Scripts/SwordsCollision.cs
+++ Assets/Scripts/SwordsCollision.cs
@@ -73,8 +73,8 @@ public class SwordsCollision : MonoBehaviour {
             OVRInput.SetControllerVibration(0.4f, 0.4f, OVRInput.Controller.RTouch);
 
             OVRInput.SetControllerVibration(0.4f, 0.4f, OVRInput.Controller.LTouch);
-
-            Instantiate(burnPrefab, collision.contacts[0].point, Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal));
+                        
+            Instantiate(burnPrefab, new Vector3(collision.contacts[0].point.x, collision.transform.position.y, collision.contacts[0].point.z), Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal));
         }
     }
 
